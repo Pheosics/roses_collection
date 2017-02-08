9284e59a089744f6a1906f59a316a6f6cea58aae
Pheosics <Pheosics@users.noreply.github.com>
2017-02-07 17:28:39 -0700

    Mid-change break

--- Scripts/raw/scripts/functions/spell.lua
+++ Scripts/raw/scripts/functions/spell.lua
@@ -6,100 +6,134 @@
  castSpell(source,target,spell,verbose) -- Runs the scripts associated with the spell, replacing certain key strings with the appropriate numbers
 ]]
 ------------------------------------------------------------------------
-function calculateResistance(unit,spell,verbose)
+function calculateResistance(target,spell,verbose)
+ local resistance = 0
+ local unit = target
+ if not unit then return resistance end
  if tonumber(unit) then unit = df.unit.find(tonumber(unit)) end
  unitID = tostring(unit.id)
  local persistTable = require 'persist-table'
  local spellTable = persistTable.GlobalTable.roses.SpellTable
  if not spellTable[spell] then
-  if verbose then print('Not a valid spell') end
-  return 0
+  if verbose then print('Not a valid spell: '..spell) end
+  return resistance
  end
  spellTable = spellTable[spell]
- if not spellTable.Resistable then return 0 end 
- local unitTable = persistTable.GlobalTable.roses.UnitTable
- if not unitTable[unitID] then dfhack.script_environment('functions/tables').makeUnitTable(unit) end
- unitTable = unitTable[unitID]
+ if not spellTable.Resistable then return resistance end
  local getResistance = dfhack.script_environment('functions/unit').getUnit
  if spellTable.Type then typeResistance = getResistance(unit,'Resistances',spellTable.Type) end
  if spellTable.Sphere then sphereResistance = getResistance(unit,'Resistances',spellTable.Sphere) end
  if spellTable.School then schoolResistance = getResistance(unit,'Resistances',spellTable.School) end
  if spellTable.Discipline then disciplineResistance = getResistance(unit,'Resistances',spellTable.Discipline) end
  if spellTable.SubDiscipline then subdisciplineResistance = getResistance(unit,'Resistances',spellTable.SubDiscipline) end
- resistance = typeResistance + (sphereResistance+schoolResistance)/2 + (disciplineResistance+subdisciplineResistance)/2
- if spellTable.Penetrate then penetration = tonumber(spellTable.Penetrate) else penetrate = 0 end
- return resistance-penetrate
+ resistanceTable = {typeResistance,sphereResistance,schoolResistance,disciplineResistance,subdisciplineResistance}
+-- Should resistance just take the largest one?
+-- resistance = table.sort(resistanceTable)[#resistanceTable]
+-- Or should it be multiplicative?
+ for _,val in pairs(resistanceTable) do
+  resistance = resistance + val*(100-resistance)/100
+ end
+-- Or should it just all add together?
+-- resistance = typeResistance+sphereResistance+schoolResistance+disciplineResistance+subdisciplineResistance
+ return resistance
+end
+
+function calculatePenetration(source,spell,verbose)
+ local penetrate = 0
+ local unit = source
+ if not unit then return penetrate end
+ if tonumber(unit) then unit = df.unit.find(tonumber(unit)) end
+ unitID = tostring(unit.id)
+ local persistTable = require 'persist-table'
+ local spellTable = persistTable.GlobalTable.roses.SpellTable
+ if not spellTable[spell] then
+  if verbose then print('Not a valid spell: '..spell) end
+  return penetrate
+ end
+ spellTable = spellTable[spell]
+ local spellPenetrate = spellTable.Penetrate or 0
+ local getPenetrate = dfhack.script_environment('functions/unit').getUnit
+ if spellTable.Type then typePenetrate = getPenetrate(unit,'Stats',spellTable.Type..'_CASTING_PENETRATION') end
+ if spellTable.Sphere then spherePenetrate = getPenetrate(unit,'Stats',spellTable.Sphere..'_CASTING_PENETRATION') end
+ if spellTable.School then schoolPenetrate = getPenetrate(unit,'Stats',spellTable.School..'_CASTING_PENETRATION') end
+ if spellTable.Discipline then disciplinePenetrate = getPenetrate(unit,'Stats',spellTable.Discipline..'_CASTING_PENETRATION') end
+ if spellTable.SubDiscipline then subdisciplinePenetrate = getPenetrate(unit,'Stats',spellTable.SubDiscipline..'_CASTING_PENETRATION') end
+ penetrateTable = {typePenetrate,spherePenetrate,schoolPenetrate,disciplinePenetrate,subdisciplinePenetrate}
+-- Should penetration just take the largest one?
+-- penetrate = table.sort(penetrateTable)[#penetrateTable]
+-- Or should it be multiplicative?
+ for _,val in pairs(penetrateTable) do
+  penetrate = penetrate + val*(100-penetrate)/100
+ end
+-- Or should it just all add together?
+-- penetrate = typePenetrate+spherePenetrate+schoolPenetrate+disciplinePenetrate+subdisciplinePenetrate
+ return penetrate
+end
+
+function calculateHitChance(source,target,spell,verbose)
+ local hitchance = 100
+ if not source or not target then return hitchance end
+ if tonumber(source) then source = df.unit.find(tonumber(source)) end
+ if tonumber(target) then target = df.unit.find(tonumber(target)) end
+ local persistTable = require 'persist-table'
+ local spellTable = persistTable.GlobalTable.roses.SpellTable
+ if not spellTable[spell] then
+  if verbose then print('Not a valid spell: '..spell) end
+  return hitchance
+ end
+ spellTable = spellTable[spell]
 end
 
 function Spell(source,target,spell,verbose)
  local persistTable = require 'persist-table'
  local spellTable = persistTable.GlobalTable.roses.SpellTable
  if not spellTable[spell] then
-  if verbose then print('Not a valid spell') end
+  if verbose then print('Not a valid spell: '..spell) end
   return
  end
  spellTable = spellTable[spell]
  
- if tonumber(source) then source = df.unit.find(tonumber(source)) end
  if source then 
-  sourceID = tostring(source.id) 
+  if tonumber(source) then source = df.unit.find(tonumber(source)) end
+  sourceID = tostring(source.id)
  else
-  print('No valid source declared')
-  return
+  sourceID = nil
  end
- if tonumber(target) then target = df.unit.find(tonumber(target)) end
  if target then
+  if tonumber(target) then target = df.unit.find(tonumber(target)) end
   targetID = tostring(target.id)
  else
   targetID = nil
  end
  
- local unitTable = persistTable.GlobalTable.roses.UnitTable
- if not unitTable[sourceID] then dfhack.script_environment('functions/tables').makeUnitTable(source) end
- unitTable = unitTable[sourceID]
- 
----- check for casting speed buffs/debuffs
--- if not unitTable.Stats.CastingSpeed then dfhack.script_environment('functions/tables').makeUnitTableStat(source,'CastingSpeed') end
--- speedTable = unitTable.Stats.CastingSpeed
--- speed = 100 - tonumber(speedTable.Base) - tonumber(speedTable.Change) - tonumber(speedTable.Item) - tonumber(speedTable.Class)
- 
----- check for casting skill and determine exhaustion
--- if not unitTable.Skills['SPELL_CASTING'] then dfhack.script_environment('functions/tables').makeUnitTableSkill(source,'SPELL_CASTING') end
--- if unitTable.Skills['SPELL_CASTING'] and spellTable.CastExhaustion then
---  castTable = unitTable.Skills['SPELL_CASTING']
---  castSkill = tonumber(castTable.Base) + tonumber(castTable.Change) + tonumber(castTable.Item) + tonumber(castTable.Class)
---  if castSkill < 0 then castSkill = 0 end
---  if castSkill > 20 then castSkill = 20 end
---  exhaustion = (100 - 4*castSkill)/100
---  exhaustion = exhaustion*tonumber(spellTable.CastExhaustion)
--- else
---  exhaustion = 0
--- end
-  
- speedPerc = dfhack.script_environment('functions/misc').fillEquation(source,nil,'CASTING_SPEED_PERC')
- exaustion = dfhack.script_environment('functions/misc').fillEquation(source,nil,'CASTING_EXHAUSTION_MODIFIER')
- if spellTable.Type and spellTable.CanCrit then
-  if spellTable.Type == 'PHYSICAL' then
-   critChance = dfhack.script_environment('functions/misc').fillEquation(source,nil,'PHYSICAL_CRIT_CHANCE')
-  else
+ if sourceID then
+  local unitTable = persistTable.GlobalTable.roses.UnitTable
+  if not unitTable[sourceID] then dfhack.script_environment('functions/tables').makeUnitTable(source) end
+  unitTable = unitTable[sourceID]
+  speedPerc = dfhack.script_environment('functions/misc').fillEquation(source,nil,'CASTING_SPEED_PERC')
+  exaustion = dfhack.script_environment('functions/misc').fillEquation(source,nil,'CASTING_EXHAUSTION_MODIFIER')
+  if spellTable.Type and spellTable.CanCrit then
+   critChance = dfhack.script_environment('functions/misc').fillEquation(source,nil,spellTable.Type..'_CRIT_CHANCE')
+  elseif spellTable.CanCrit
    critChance = dfhack.script_environment('functions/misc').fillEquation(source,nil,'MAGICAL_CRIT_CHANCE')
+  else
+   critChange = 0
   end
  else
-  critChance = dfhack.script_environment('functions/misc').fillEquation(source,nil,'MAGICAL_CRIT_CHANCE')
+  speedPerc = 100
  end
-      
  -- process spell -> Set delay (if necessary) -> Cast Spell -> Gain Experience -> Gain Skill -> Add Exhaustion
  if spellTable.CastTime and speedPerc > 0 then
   speed = math.floor((speedPerc/100)*tonumber(spellTable.CastTime))
   if speed == 0 then speed = 1 end
-  dfhack.run_command('unit/action-change -unit '..sourceID..' -action All -interaction All -timer '..tostring(speed))
+  if sourceID then dfhack.run_command('unit/action-change -unit '..sourceID..' -action All -interaction All -timer '..tostring(speed)))
   dfhack.script_environment('persist-delay').environmentDelay(speed,'functions/class','castSpell',{sourceID,targetID,spell})
  else
   castSpell(source,target,spell)
  end
- if exhaustion > 0 then dfhack.script_environment('functions/unit').changeCounter(sourceID,'exhaustion',exhaustion) end
- if spellTable.ExperienceGain then dfhack.script_environment('functions/class').addExperience(sourceID,tonumber(spellTable.ExperienceGain)) end
- if spellTable.SkillGain then
+ if exhaustion > 0 and sourceID then dfhack.script_environment('functions/unit').changeCounter(sourceID,'exhaustion',exhaustion) end
+ if spellTable.ExperienceGain and sourceID then dfhack.script_environment('functions/class').addExperience(sourceID,tonumber(spellTable.ExperienceGain)) end
+ if spellTable.SkillGain and sourceID then
   for _,skill in pairs(spellTable.SkillGain._children) do
    amount = spellTable.SkillGain[skill]
    dfhack.script_environment('functions/unit').changeSkill(sourceID,skill,amount)
@@ -111,24 +145,34 @@ function castSpell(source,target,spell,verbose)
  local persistTable = require 'persist-table'
  local spellTable = persistTable.GlobalTable.roses.SpellTable
  if not spellTable[spell] then
-  if verbose then print('Not a valid spell') end
+  if verbose then print('Not a valid spell: '..spell) end
   return
  end
  spellTable = spellTable[spell]
  
- if tonumber(source) then source = df.unit.find(tonumber(source)) end
+ if spellTable.AutoCalculateResistance then
+  resistance = computeResistance(target,spell,verbose)
+  penetration = computePenetration(source,spell,verbose)
+  resMod = 1 - (resistance-penetration)/100
+  if resMod > 1 then resMod = 1 end
+  if resMod < 0 then resMod = 0 end
+ else
+  resMod = 1
+ end
+ 
  if source then
+  if tonumber(source) then source = df.unit.find(tonumber(source)) end
   sourceID = tostring(source.id)
  else
   sourceID = "\\-1"
  end
- if tonumber(target) then target = df.unit.find(tonumber(target)) end
  if target then
+  if tonumber(target) then target = df.unit.find(tonumber(target)) end
   targetID = tostring(target.id)
  else
   targetID = "\\-1"
  end
- 
+
  for _,i in pairs(spellTable.Script._children) do
   script = spellTable.Script[i]
   script = script:gsub('SPELL_TARGET',targetID)
@@ -137,6 +181,8 @@ function castSpell(source,target,spell,verbose)
   script = script:gsub('SPELL_SOURCE',sourceID)
   script = script:gsub('\\SOURCE_ID',sourceID)
   script = script:gsub('\\ATTACKER_ID',sourceID)
+  script = script:gsub('SPELL_RES_MOD',resMod)
+  script = script:gsub('SPELL_RESISTANCE',resMod)
   dfhack.run_command(script)
  end
 end
