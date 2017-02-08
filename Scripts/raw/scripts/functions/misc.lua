60c7d2306ee450fda4f30c8074ccdbb4f60938be
Pheosics <Pheosics@users.noreply.github.com>
2017-02-08 16:22:50 -0700

    Update misc.lua
    Added the fillEquation script

--- Scripts/raw/scripts/functions/misc.lua
+++ Scripts/raw/scripts/functions/misc.lua
@@ -6,6 +6,133 @@
  checkCounter(counter,extra) - Checks if a counter exists, returns true/false
  getCounter(counter,extra) - Get the value of a certain counter, returns a number
 ]]
+function fillEquation(source,target,spell,equation)
+ local utils = require 'utils'
+ local split = utils.split_string 
+ equation = string.upper(equation)
+ local persistTable = require 'persist-table'
+ local equationTable = persistTable.GlobalTable.roses.baseTable.Equations
+ if equationTable[equation] then
+  spellSourceEquations = { MAGICAL_EXHAUSTION_MODIFIER = true,
+                           MAGICAL_SPEED_PERC = true,
+                           MAGICAL_HIT_CHANCE = true,
+                           MAGICAL_CRIT_CHANCE = true,
+                           MAGICAL_CRIT_BONUS = true,
+                           PHYSICAL_EXHAUSTION_MODIFIER = true,
+                           PHYSICAL_SPEED_PERC = true,
+                           PHYSICAL_HIT_CHANCE = true,
+                           PHYSICAL_CRIT_CHANCE = true,
+                           PHYSICAL_CRIT_BONUS = true }
+  spellTargetEquations = { DODGE_CHANCE = true }
+  local temp = equationTable[equation]
+  if spellSourceEquations[equation] then
+   temp = temp:gsub('ATTRIBUTE','SOURCE.ATTRIBUTE')
+   temp = temp:gsub('SKILL','SOURCE.SKILL')
+   temp = temp:gsub('STAT','SOURCE.STAT')
+   temp = temp:gsub('RESISTANCE','SOURCE.RESISTANCE')
+  elseif spellTargetEquations[equation] then
+   temp = temp:gsub('ATTRIBUTE','TARGET.ATTRIBUTE')
+   temp = temp:gsub('SKILL','TARGET.SKILL')
+   temp = temp:gsub('STAT','TARGET.STAT')
+   temp = temp:gsub('RESISTANCE','TARGET.RESISTANCE')
+  end
+  equation = temp
+ end
+ if source then
+  if tonumber(source) then source = df.unit.find(tonumber(source)) end
+  sourceID = source.id
+ end
+ if target then
+  if tonumber(target) then target = df.unit.find(tonumber(target)) end
+  targetID = target.id
+ end
+ if spell then
+  local spellTable = persistTable.GlobalTable.roses.SpellTable
+  if not spellTable[spell] then
+   spell = nil
+  end
+ end
+ local unitFunctions = dfhack.script_environment('functions/unit')
+ local spellFunctions = dfhack.script_environment('functions/spell')
+ for _,check in pairs({'SOURCE','TARGET'}) do
+  if check == 'SOURCE' then
+   if not sourceID then
+    break
+   else
+    unitID = sourceID
+   end
+  elseif check == 'TARGET' then
+   if not targetID then
+    break
+   else
+    unitID = targetID
+   end
+  end
+  while equation:find(check) do
+   look = string.match(equation..'+',check..".(.-)[+%-*/]")
+   array = split(look,"%.")
+   if array[1] == 'ATTRIBUTE' then
+    if array[2] == 'WEIGHT' then
+     value = unitFunctions.calculateWieght(unitID,false)
+    elseif array[2] == 'FULL_WEIGHT' then
+     value = unitFunctions.calculateWieght(unitID,true)
+    elseif array[2] == 'PRIMARY' and spell then
+     value = spellFunctions.calculateAttribute(sourceID,spell,'PRIMARY',check)
+    elseif array[2] == 'SECONDARY' and spell then
+     value = spellFunctions.calculateAttribute(sourceID,spell,'SECONDARY',check)
+    else
+     value = unitFunctions.getUnit(unitID,'Attributes',array[2])
+    end
+   elseif array[1] == 'SKILL' then
+    if array[2] == 'WEAPON' then
+     value = unitFunctions.getWeaponSkill(unitID)
+    elseif array[2] == '!_SPELL_CASTING' and spell then
+     value = spellFunctions.calculateSkill(unitID,spell,'SPELL_CASTING')
+    else
+     value = unitFunctions.getUnit(unitID,'Skills',array[2])
+    end
+   elseif array[1] == 'STAT' then
+    if array[2] == '!_HIT_CHANCE' and spell then
+     value = spellFunctions.calculateStat(unitID,spell,'HIT_CHANCE')
+    elseif array[2] == '!_CASTING_SPEED' and spell then
+     value = spellFunctions.calculateStat(unitID,spell,'CASTING_SPEED')
+    elseif array[2] == '!_ATTACK_SPEED' and spell then
+     value = spellFunctions.calculateStat(unitID,spell,'ATTACK_SPEED')
+    elseif array[2] == '!_CRITICAL_CHANCE' and spell then
+     value = spellFunctions.calculateStat(unitID,spell,'CRITICAL_CHANCE')
+    elseif array[2] == '!_CRITICAL_BONUS' and spell then
+     value = spellFunctions.calculateStat(unitID,spell,'HIT_CHANCE')
+    elseif array[2] == '!_SKILL_PENETRATION' and spell then
+     value = spellFunctions.calculateStat(unitID,spell,'SKILL_PENETRATION')
+    else
+     value = unitFunctions.getUnit(unitID,'Stats',array[2])
+    end
+   elseif array[1] == 'RESISTANCE' then
+    if array[2] == '!_TDDS' then
+     value = spellFunctions.calculateResistance(unitID,spell)
+    else
+     value = unitFunctions.getUnit(unitID,'Resistances',array[2])
+    end
+   elseif array[1] == 'TRAIT' then
+    value = unitFunctions.getUnit(unitID,'Traits',array[2])
+   elseif array[1] == 'COUNTER' then
+    value = unitFunction.getCounter(unitID,string.lower(array[2]))
+   end
+   equation = equation:gsub(string.match(equation..'+',"("..check..".-)[+%-*/]"),tostring(value)) 
+  end
+ end
+ while equation:find('EQUATION') do
+  look = string.match(equation..'+',"EQUATION.(.-)[+%-*/]")
+  array = split(look,"%.")
+  value = fillEquation(sourceID,targetID,spell,array[1])
+  equation = equation:gsub(string.match(equation..'+',"(EQUATION.-)[+%-*/]"),tostring(value)) 
+ end
+ 
+ equals = assert(load("return "..equation))
+ value = equals()
+ return value 
+end
+
 function getChange(current,value,mode)
  local change = 0
  if mode == 'Fixed' or mode == 'fixed' then
