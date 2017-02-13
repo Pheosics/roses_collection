cc8981308af62e786dff485ab0aa57618fdcd1d2
Pheosics <Pheosics@users.noreply.github.com>
2017-02-13 14:55:40 -0700

    Update periodic-check.lua
    Moved enhanced creature checks into functions/enhanced

--- Scripts/raw/scripts/base/periodic-check.lua
+++ Scripts/raw/scripts/base/periodic-check.lua
@@ -19,19 +19,7 @@ for id,unit in pairs(df.globa.world.units.active) do
  end
  -- Enhanced Creature Checks
  if roses.EnhancedCreatureTable and not unitTable.Enhanced then
-  local creatureID = df.global.world.raws.creatures.all[unit.race].creature_id
-  if roses.EnhancedCreatureTable[creatureID] then
-   local creatureTable = roses.EnhancedCreatureTable[creatureID]
-   local enhancedFunc = dfhack.script_environment('functions/enhanced')
-   if creatureTable.Size then enhancedFunc.setSize(unit,creatureTable.Size) end
-   if creatureTable.Attributes then enhancedFunc.setAttributes(unit,creatureTable.Attributes) end
-   if creatureTable.Skills then enhancedFunc.setSkills(unit,creatureTable.Skills) end
-   if creatureTable.Stats then enhancedFunc.setStats(unit,creatureTable.Stats) end
-   if creatureTable.Resistances then enhancedFunc.setResistances(unit,creatureTable.Resistances) end
-   if creatureTable.Classes and classNeeded then enhancedFunc.setClass(unit,creatureTable.Classes) end
-   if creatureTable.Interactions then enhancedFunc.setInteractions(unit,creatureTable.Classes) end
-   unitTable.Enhanced = 'true'
-  end
+  dfhack.script_environment('functions/enhanced').enhanceCreature(unit)
  end
 end
   
