cf3ecb4240ca05d07f60f63e2208cfdc46311c81
Miles A. Engel <mengel@wistler.lanl.gov>
2017-06-20 13:43:38 -0600

    Started working on the create unit functions

--- Scripts/raw/scripts/functions/unit.lua
+++ Scripts/raw/scripts/functions/unit.lua
@@ -49,7 +49,13 @@
  getCounter(unit,counter)
 
  create(location,raceID,casteID,refUnit,side,name,dur,track,syndrome,cb_id)
+ createHistFig()
+ createName()
+ createNemesis()
+ createUnit()
+
  makeProjectile(unit,velocity)
+
  findUnit(search)
 ]]
 ---------------------------------------------------------------------------------------
@@ -269,7 +275,7 @@ function trackTrait(unit,kind,current,change,value,dur,alter,syndrome,cb_id)
 end
 
 -------------- Complex Tracking (Create, Side, Transform) -------------------
-function trackCreate(unit,summoner,dur,alter,syndrome,cb_id)
+function trackCreate(unit,summoner,dur,syndrome,cb_id)
  persistTable = require 'persist-table'
  if not persistTable.GlobalTable.roses then
   return
@@ -1968,81 +1974,248 @@ end
 ------ The following function just reference modtools/create-unit, this allows --------
 ------ me to change certain features of that code without altering it directly --------
 ---------------------------------------------------------------------------------------
-function create(location,raceID,casteID,refUnit,side,name,dur,track,syndrome,cb_id)
- if tonumber(refUnit) then refUnit = df.unit.find(tonumber(refUnit)) end
+function create(pos,creature,options)
+ script = require 'gui.script'
 
- id = dfhack.script_environment('modtools/create-unit').createUnit(raceID,casteID)
- unit = df.unit.find(id)
- 
- civ_id,group_id = changeSide(unit,refUnit,side,0)
- dur = tonumber(dur) or 0
- if side == 'Civilian' then
-  dfhack.script_environment('modtools/create-unit').createNemesis(unit,civ_id,group_id)
- elseif side == 'Ally' then
-  dfhack.script_environment('modtools/create-unit').createNemesis(unit,civ_id,-1)
+ options = options or {}
+ number = options.number or 1
+ reference = options.reference or 'LOCAL'
+ side = options.side or 'NEUTRAL'
+ name = options.name
+ dur = options.duration or 0
+ track = options.track
+ syndrome = options.syndrome
+ equip = options.equipment
+ skills = options.skills
+ classes = options.classes
+
+ persistTable = require 'persist-table'
+ roses = persistTable.GlobalTable.roses
+ -- Get location for unit placement (either x,y,z coordinates or a special position token)
+ local location = {}
+ if pos == 'EDGE' then
+  -- So far let's just assume it's an x,y,z coordinate
+  return
+ else
+  location.x = pos.x or pos[1]
+  location.y = pos.y or pos[2]
+  location.z = pos.z or pos[3]
  end
- 
- if name then
-  entities = {}
-  for i,x in pairs(df.global.world.raws.entities) do
-   entities[x.code] = true
-  end
-  if name == 'Random' then
-   entityRaw = dfhack.script_environment('functions/misc').permute(entities)[1]
-   dfhack.script_environment('modtools/create-unit').nameUnit(id, entityRaw, civ_id)
-  elseif entities[name] then
-   dfhack.script_environment('modtools/create-unit').nameUnit(id, name, civ_id)
-  else
-   unit.name.first_name = name
-   unit.name.has_name = true
-   unit.status.current_soul.name.first_name = name
-   unit.status.current_soul.name.has_name = true
-   if unit.hist_figure_id ~= -1 then
-    local histfig = df.historical_figure.find(unit.hist_figure_id)
-    histfig.name.first_name = name
-    histfig.name.has_name = true
+
+ -- Get creature's race and caste
+ local race = nil
+ local caste = nil
+ if creature then
+  race_token = split(creature,':')[1]
+  caste_token = split(creature,':')[2]
+  for i,x in ipairs(df.global.world.raws.creatures.all) do
+   if x.creature_id == race_token then
+    raceID = i
+    race = x
+    break
    end
   end
+  if not race then
+   print('Invalid race')
+   return
+  end
+  if caste_token == 'RANDOM' then
+   caste = dfhack.script_environment('functions/misc').permute(race.caste)[1]
+   casteID = caste.id
+   casteIDs = {}
+   for i,x in pairs(race.caste) do
+    casteIDs[i] = x.id
+   end
+  else
+   for i,x in ipairs(race.caste) do
+    if x.caste_id == caste_token then
+     casteID= i
+     caste = x
+     break
+    end
+   end
+  end
+  if not caste then
+   print('Invalid caste')
+   return
+  end
  else
-  unit.name.has_name = false
-  if unit.status.current_soul then
-    unit.status.current_soul.name.has_name = false
+  print('No unit declared')
+  return
+ end
+
+ -- Get civ_id and group_id (actually do that per unit basis)
+ local civ_id = -1
+ local group_id = -1
+
+ -- Start to create units
+ function createMultiple()
+  for n = 1, number do
+   df.global.pause_state = true
+   -- Create Unit
+   local id
+   if caste_token == 'RANDOM' then
+    local index = math.random(0,#casteIDs-1)
+    id = createUnit(raceID,casteIDs[index],location)
+   else
+    id = createUnit(raceID,casteID,location)
+   end
+   unit = df.unit.find(id)
+   -- Assign ids (e.g. civ_id, population_id, etc...)
+   civ_id, group_id = changeSide(unit,reference,side,0)
+   if civ_id ~= -1 then
+    createNemesis(unit,civ_id,group_id)
+   end
+   -- Assign unit a name
+   if name then
+    createName(unit,name)
+   end
+   -- Equip unit with items
+   if equip then
+    items,parts,modes = createEquipment(unit,equip)
+    for i,item in pairs(items) do
+     dfhack.script_environment('functions/item').equip(item,unit,parts[i],modes[i])
+    end
+   end
+   -- Add skills to unit
+   if skills then
+    skill, level = createSkills(unit,skills)
+    for i,x in pairs(skill) do
+     changeSkill(unit,x,level[i])
+    end
+   end
+   -- Assign a class to unit (based on the Class System)
+   if classes then
+    class, level = createClass(unit,classes)
+    if class then dfhack.script_environment('functions/class').changeClass(class,level) end
+   end
+   -- Enhance unit (based on the Enhanced System)
+   if roses then
+    if safe_index(roses,'EnhancedCreatureTable') then
+     dfhack.script_environment('functions/enhanced').enhanceCreature(unit)
+    end
+   end
+   -- Add a syndrome, set up a duration, and track changes
+   if syndrome then changeSyndrome(unit,syndrome,'add') end
+   if dur > 0 then cb_id = dfhack.script_environment('persist-delay').environmentDelay(dur,'functions/unit','removal',{unit.id,'created',nil}) end
+   if track then trackCreate(unit,reference,dur,syndrome,cb_id) end
+ 
+   df.global.pause_state = false
+   script.sleep(5,'ticks')
   end
  end
- 
- if location then move(id,location) end
- if syndrome then changeSyndrome(unit,syndrome,'add') end
- if dur > 0 then cb_id = dfhack.script_environment('persist-delay').environmentDelay(dur,'functions/unit','removal',{unit.id,'created',nil}) end
- if track then trackCreate(unit,refUnit,dur,alter,syndrome,cb_id) end
- 
- return unit
+
+ script.start(createMultiple)
+ return
 end
 
-function createHistFig(trgunit,he,he_group) --Taken from modtools/create-unit
+function createClass(unit,classes)
+ local class = nil
+ local level = nil
+
+ utils = require 'utils'
+ split = utils.split_string
+ class = split(class,':')
+ if class[1] == 'CIVILIZATION' then
+  -- Classes avaiable to the civilization (using Civilization System) will go here
+ elseif class[1] == 'ITEMS' then
+  -- Classes based on equipped items will be chosen
+ elseif class[1] == 'RANDOM' then
+  -- A random class will be chosen
+ elseif class[1] == 'TEMPLATE' then
+  -- Classes from a template file will go here
+ else
+  -- A specific class will go here
+ end
+
+ return class, level
+end
+
+function createEquipment(unit,equip)
+ utils = require 'utils'
+ split = utils.split_string
+
+ equipment = split(equip,':')
+ items = {}
+ parts = {}
+ modes = {}
+
+ entities = {}
+ for _,x in pairs(df.global.world.raws.entities) do
+  entities[x.code] = x
+ end
+ if entities[equipment[1]] then
+  -- Item Options: Based off of entity raws
+  -- Mat Options: Random
+  raw = entities[equipment[1]]
+  availItems = {'weapon'=raw.weapon_id,'armor'=raw.armor_id,'helm'=raw.helm_id,'gloves'=raw.gloves_id,'shoes'=raw.shoes_id,
+                'pants'=raw.pants_id,'shield'=raw.shield_id,'ammo'=raw.ammo_id}
+  value = tonumber(equipment[2])
+ elseif equipment[1] == 'CIV' then
+  -- Item Options: Items available to the units civ
+  -- Mat Options: Materials available to the units civ
+  civ = df.historic_entity.find(unit.civ_id)
+  rsr = civ.resources
+  mtl = civ.resources.metal
+  availItems = {'weapon'=rsr.weapon_type,'armor'=rsr.armor_type,'helm'=rsr.helm_type,'gloves'=rsr.gloves_type,'shoes'=rsr.shoes_type,
+                'pants'=rsr.pants_type,'shield'=rsr.ammo_type,'ammo'=rsr.ammo_type}
+  availMats  = {'weapon'=mtl.weapon,'armor'=mtl.armor,'ammo'=mtl.ammo,'leather'=rsr.organic.leather,'cloth'=rsr.organic.fiber}
+  value = tonumber(equipment[2])
+ elseif equipment[1] == 'TEMPLATE' then
+  -- Item Options: Items read from a template
+  -- Mat Options: Materials read from a template
+  template = equipment[2]
+  persistTable = require 'persist-table'
+  roses = persistTable.GlobalTable.roses
+  if safe_index(roses,'EquipmentTemplates',template) then
+   tmp = roses.EquipmentTemplates[template]
+   availItems = {'weapon'=tmp.Weapons,'armor'=tmp.Armor,'helm'=tmp.Helms,'gloves'=tmp.Gloves,'shoes'=tmp.Shoes,
+                 'pants'=tmp.Pants,'shield'=tmp.Shields,'ammo'=tmp.Ammo}
+   availMats  = {'weapon'=tmp.WeaponMats,'armor'=tmp.ArmorMats,'ammo'=tmp.AmmoMats,'leather'=tmp.Leathers,'cloth'=tmp.Cloths}
+   value = tmp.Value
+  else
+   return items, parts, modes
+  end
+ elseif equipment[1] == 'RANDOM' then
+  -- Item Options: Random
+  -- Mat Options: Random
+  value = tonumber(equipment[2])
+ elseif equipment[1] == 'UNIFORM' then -- Unsure if this will actually work
+  -- Item Options: Items from a uniform
+  -- Mat Options: Materials from a uniform
+  value = 0
+ end
+
+ return items, parts, modes 
+end
+
+function createHistFig(unit,he,he_group) --Taken from modtools/create-unit
+ if tonumber(unit) then unit = df.unit.find(tonumber(unit)) end
+
  local hf=df.historical_figure:new()
  hf.id=df.global.hist_figure_next_id
- hf.race=trgunit.race
- hf.caste=trgunit.caste
- hf.profession = trgunit.profession
- hf.sex = trgunit.sex
+ hf.race=unit.race
+ hf.caste=unit.caste
+ hf.profession = unit.profession
+ hf.sex = unit.sex
  df.global.hist_figure_next_id=df.global.hist_figure_next_id+1
  hf.appeared_year = df.global.cur_year
 
- hf.born_year = trgunit.birth_year
- hf.born_seconds = trgunit.birth_time
- hf.curse_year = trgunit.curse_year
- hf.curse_seconds = trgunit.curse_time
- hf.birth_year_bias = trgunit.birth_year_bias
- hf.birth_time_bias = trgunit.birth_time_bias
- hf.old_year = trgunit.old_year
- hf.old_seconds = trgunit.old_time
+ hf.born_year = unit.birth_year
+ hf.born_seconds = unit.birth_time
+ hf.curse_year = unit.curse_year
+ hf.curse_seconds = unit.curse_time
+ hf.birth_year_bias = unit.birth_year_bias
+ hf.birth_time_bias = unit.birth_time_bias
+ hf.old_year = unit.old_year
+ hf.old_seconds = unit.old_time
  hf.died_year = -1
  hf.died_seconds = -1
- hf.name:assign(trgunit.name)
- hf.civ_id = trgunit.civ_id
- hf.population_id = trgunit.population_id
+ hf.name:assign(unit.name)
+ hf.civ_id = unit.civ_id
+ hf.population_id = unit.population_id
  hf.breed_id = -1
- hf.unit_id = trgunit.id
+ hf.unit_id = unit.id
 
  df.global.world.history.figures:insert("#",hf)
 
@@ -2052,7 +2225,6 @@ function createHistFig(trgunit,he,he_group) --Taken from modtools/create-unit
  hf.info.unk_14.unk_18 = -1; hf.info.unk_14.unk_1c = -1
  -- set values that seem related to state and do event
  --change_state(hf, dfg.ui.site_id, region_pos)
-
 
  --lets skip skills for now
  --local skills = df.historical_figure_info.T_skills:new() -- skills snap shot
@@ -2068,75 +2240,103 @@ function createHistFig(trgunit,he,he_group) --Taken from modtools/create-unit
   he_group.hist_figures:insert('#', hf)
   hf.entity_links:insert("#",{new=df.histfig_entity_link_memberst,entity_id=he_group.id,link_strength=100})
  end
- trgunit.flags1.important_historical_figure = true
- trgunit.flags2.important_historical_figure = true
- trgunit.hist_figure_id = hf.id
- trgunit.hist_figure_id2 = hf.id
+ unit.flags1.important_historical_figure = true
+ unit.flags2.important_historical_figure = true
+ unit.hist_figure_id = hf.id
+ unit.hist_figure_id2 = hf.id
 
- hf.entity_links:insert("#",{new=df.histfig_entity_link_memberst,entity_id=trgunit.civ_id,link_strength=100})
+ hf.entity_links:insert("#",{new=df.histfig_entity_link_memberst,entity_id=unit.civ_id,link_strength=100})
 
  --add entity event
  local hf_event_id=df.global.hist_event_next_id
  df.global.hist_event_next_id=df.global.hist_event_next_id+1
- df.global.world.history.events:insert("#",{new=df.history_event_add_hf_entity_linkst,year=trgunit.birth_year,
- seconds=trgunit.birth_time,id=hf_event_id,civ=hf.civ_id,histfig=hf.id,link_type=0})
+ df.global.world.history.events:insert("#",{new=df.history_event_add_hf_entity_linkst,year=unit.birth_year,
+                                            seconds=unit.birth_time,id=hf_event_id,civ=hf.civ_id,histfig=hf.id,
+                                            link_type=0})
  return hf
 end
 
-function createName() -- Taken from modtools/create-unit
- --pick a random appropriate name
- --choose three random words in the appropriate things
- local unit = df.unit.find(id)
- local entity_raw
- if entityRawName then
-  for k,v in ipairs(df.global.world.raws.entities) do
-   if v.code == entityRawName then
-    entity_raw = v
+function createName(unit,name) -- Taken from modtools/create-unit
+ if tonumber(unit) then unit = df.unit.find(tonumber(unit)) end
+
+ entityRaws = nil
+ if name == 'Random' then
+  -- Take a random entity and assign a name based off of that entity
+  local index = math.random(0,#df.global.world.raws.entities-1)
+  entityRaws = df.global.world.raws.entities[index]
+ elseif name == 'Animal' then
+  -- For now don't name animals
+  return
+ else
+  -- Pick name from specific entity or from current civs entity 
+  entities = {}
+  for i,x in pairs(df.global.world.raws.entities) do
+   entities[x.code] = x
+  end
+  if entities[name] then
+   entityRaws = entities[name]
+  elseif name == 'Civ' then
+   if unit.civ_id ~= -1 then
+    entityRaws = df.historical_entity.find(unit.civ_id).entity_raw
+   else
+    --No civ to assign name based on
+    return
+   end
+  end
+ end
+ 
+ -- If an entity_raw was found for the declared name variable then use that
+ -- Else assume the name variable represents an actual name (e.g. "John Robert Smith")
+ if entityRaws then
+  translation = entityRaws.translation
+  translationIndex = -1
+  for i,x in ipairs(df.global.world.raws.language.translations) do
+   if x.name == translation then
+    translationIndex = i
     break
    end
   end
- else
-  local entity = df.historical_entity.find(civ_id)
-  entity_raw = entity.entity_raw
- end
-
- if not entity_raw then
-  error('entity raw = nil: ', id, entityRawName, civ_id)
- end
-
- local translation = entity_raw.translation
- local translationIndex
- for k,v in ipairs(df.global.world.raws.language.translations) do
-  if v.name == translation then
-   translationIndex = k
-   break
+  local language_word_table = entityRaws.symbols.symbols1[0]
+  function randomWord()
+   local index = math.random(0, #language_word_table.words[0] - 1)
+   return index
   end
+  local firstName = randomWord()
+  local lastName1 = randomWord()
+  local lastName2 = randomWord()
+  local language = nil
+  for _,lang in pairs(df.global.world.raws.language.translations) do
+   if lang.name == entityRaws.translation then
+    language = lang
+   end
+  end
+  if language then
+   first = language.words[firstName].value
+  else
+   first = df.language_word.find(language_word_table.words[0][firstName]).forms[language_word_table.parts[0][firstName]]
+  end
+  word0 = language_word_table.words[0][lastName1]
+  part0 = language_word_table.parts[0][lastName1]
+  word1 = language_word_table.words[0][lastName2]
+  part1 = language_word_table.words[0][lastName2]
+ else
+  utils = require 'utils'
+  split = utils.split_string
+  nameTable = split(name,' ')
+  first = nameTable[1] or ''
+  word0 = nameTable[2] or ''
+  part0 = 'Noun'
+  word1 = nameTable[3] or ''
+  part1 = 'Noun'
+  translationIndex = -1
  end
- --translation = df.language_translation.find(translation)
- local language_word_table = entity_raw.symbols.symbols1[0] --educated guess
- function randomWord()
-  local index = math.random(0, #language_word_table.words[0] - 1)
-  return index
- end
- local firstName = randomWord()
- local lastName1 = randomWord()
- local lastName2 = randomWord()
+
  local name = unit.status.current_soul.name
- name.words[0] = language_word_table.words[0][lastName1]
- name.parts_of_speech[0] = language_word_table.parts[0][lastName1]
- name.words[1] = language_word_table.words[0][lastName2]
- name.parts_of_speech[1] = language_word_table.parts[0][lastName2]
- local language = nil
- for _, lang in pairs(df.global.world.raws.language.translations) do
-  if lang.name == entity_raw.translation then
-   language = lang
-  end
- end
- if language then
-  name.first_name = language.words[firstName].value
- else
-  name.first_name = df.language_word.find(language_word_table.words[0][firstName]).forms[language_word_table.parts[0][firstName]]
- end
+ name.words[0] = word0
+ name.parts_of_speech[0] = part0
+ name.words[1] = word1
+ name.parts_of_speech[1] = part1
+ name.first_name = first
  name.has_name = true
  name.language = translationIndex
  unit.name:assign(name)
@@ -2146,7 +2346,9 @@ function createName() -- Taken from modtools/create-unit
  end
 end
 
-function createNemesis(trgunit,civ_id,group_id) -- Taken from modtools/create-unit
+function createNemesis(unit,civ_id,group_id) -- Taken from modtools/create-unit
+ if tonumber(unit) then unit = df.unit.find(tonumber(unit)) end
+
  local function  allocateNewChunk(hist_entity)
   hist_entity.save_file_id=df.global.unit_chunk_next_id
   df.global.unit_chunk_next_id=df.global.unit_chunk_next_id+1
@@ -2167,8 +2369,8 @@ function createNemesis(trgunit,civ_id,group_id) -- Taken from modtools/create-un
  local nem=df.nemesis_record:new()
 
  nem.id=id
- nem.unit_id=trgunit.id
- nem.unit=trgunit
+ nem.unit_id=unit.id
+ nem.unit=unit
  nem.flags:resize(4)
   --not sure about these flags...
  --[[
@@ -2187,8 +2389,8 @@ function createNemesis(trgunit,civ_id,group_id) -- Taken from modtools/create-un
  nem.unk12=-1
  df.global.world.nemesis.all:insert("#",nem)
  df.global.nemesis_next_id=id+1
- trgunit.general_refs:insert("#",{new=df.general_ref_is_nemesisst,nemesis_id=id})
- trgunit.flags1.important_historical_figure=true
+ unit.general_refs:insert("#",{new=df.general_ref_is_nemesisst,nemesis_id=id})
+ unit.flags1.important_historical_figure=true
 
  nem.save_file_id=-1
 
@@ -2204,7 +2406,27 @@ function createNemesis(trgunit,civ_id,group_id) -- Taken from modtools/create-un
   he_group.nemesis:insert("#",nem)
  end
  allocateIds(nem,he)
- nem.figure=createFigure(trgunit,he,he_group)
+ nem.figure=createHistFig(unit,he,he_group)
+end
+
+function createSkills(unit,skills)
+ local skill = {}
+ local level = {}
+
+ utils = require 'utils'
+ split = utils.split_string
+ table = split(skills,':')
+ if table[1] == 'TEMPLATE' then
+  -- Skills from a template file will go here
+ elseif table[1] == 'ITEMS' then
+  -- Skills based on equipped items will go here
+ elseif table[1] == 'RANDOM' then
+  -- Random skills will go here
+ else
+  -- A specific skill will go here
+ end
+
+ return skill, level
 end
 
 function createUnit(race_id,caste_id,location) -- Taken from modtools/create-unit
